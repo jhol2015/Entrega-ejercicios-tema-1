3104f5c1df7eedf94a51fdbcf23c536081edc7a6
Jhol Moreira <galilleu2011@gmail.com>
2022-06-21 16:40:17 -0300

    Entrega ejercicios tema 1.

---
+++ src/com/example/MetodoMain.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class MetodoMain {
+    public static void main(String[] args) {
+        int variable1 = 5;
+        long variable2 = 10;
+        double variable3 = 10.5d;
+        boolean variable4 = false;
+        String variable5 = "José";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+
+    }
+}
